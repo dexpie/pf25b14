65cb3adbb94b4ba4199f8122001cd850c997617e
Gading Putra Priyanto (129) dexpie <gadingputra103@gmail.com>
2025-06-25 22:43:08 +0700

    Add files via upload

--- TicTacToeServer.java
+++ TicTacToeServer.java
@@ -6,7 +6,6 @@ public class TicTacToeServer {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(PORT);
         System.out.println("Server started. Waiting for players...");
-        // Terima dua client
         Socket player1 = serverSocket.accept();
         System.out.println("Player 1 connected: " + player1.getInetAddress());
         Socket player2 = serverSocket.accept();
@@ -17,11 +16,9 @@ public class TicTacToeServer {
         BufferedReader in2 = new BufferedReader(new InputStreamReader(player2.getInputStream()));
         PrintWriter out2 = new PrintWriter(player2.getOutputStream(), true);
 
-        // Variabel untuk menyimpan nickname
         final String[] nick1 = {"Player1"};
         final String[] nick2 = {"Player2"};
 
-        // Thread untuk relay dan parsing nickname
         Thread t1 = new Thread(() -> {
             try {
                 String line;
@@ -30,7 +27,6 @@ public class TicTacToeServer {
                         nick1[0] = line.substring(5);
                         out2.println("OPPONENT_NICK " + nick1[0]);
                     } else if (line.startsWith("REMATCH")) {
-                        // Relay permintaan rematch ke player 2
                         out2.println("REMATCH");
                     } else {
                         out2.println(line);
@@ -48,7 +44,6 @@ public class TicTacToeServer {
                         nick2[0] = line.substring(5);
                         out1.println("OPPONENT_NICK " + nick2[0]);
                     } else if (line.startsWith("REMATCH")) {
-                        // Relay permintaan rematch ke player 1
                         out1.println("REMATCH");
                     } else {
                         out1.println(line);
@@ -58,7 +53,6 @@ public class TicTacToeServer {
                 System.out.println("Player 2 disconnected.");
             }
         });
-        // Notifikasi ke client bahwa lawan sudah terhubung
         out1.println("OPPONENT_CONNECTED");
         out2.println("OPPONENT_CONNECTED");
         t1.start();
@@ -67,7 +61,6 @@ public class TicTacToeServer {
             t1.join();
             t2.join();
         } catch (InterruptedException e) {
-            // abaikan
         }
         player1.close();
         player2.close();
